a5ef7628d4232ac33e6fbabcd0fb193a5161ae6c
anoyoshihisa <anoyoshihisa@gmail.com>
2026-05-08 02:50:49 +0900

    update functions index

--- functions/index.js
+++ functions/index.js
@@ -206,6 +206,7 @@ exports.onPrizeImageApplyRequested = onDocumentCreated("users/{uid}/prize_image_
   await batch.commit();
   await event.data.ref.delete();
 });
+
 // --- 8. 画像削除リクエスト ---
 exports.onImageDeletionRequested = onDocumentCreated("users/{uid}/image_deletion_requests/{requestId}", async (event) => {
   const { imageName } = event.data.data();
